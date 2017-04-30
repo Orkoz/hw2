76b5b93daae212c695ed75ed2f4b9e2898e89fd5
Orkoz <kozlovskyor@gmail.com>
2017-04-30 18:45:46 +0300

    עדכון קבצי הצהרה
    היו טעיות סינטקס וגם הבנה שהגיעה מתרגול 3 שקופיות 15-16

--- flight.c
+++ flight.c
@@ -1,4 +1,33 @@
+//*************************************************************************
+//* Function name:
+//* Description:
+//* Parameters:
+//* Return Value:
+//*************************************************************************
+
+#include <AirPort_Include_File.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-createFlight (fligh_num, flight_type, destnition, emrgency)
+typedef struct flight_t{
+	int flight_num;
+	FlightType flight_type;
+	char destination[3];
+	BOOL emergency;
+	struct FLIGHT* pNext;
+}FLIGHT;
+
+FLIGHT* createFlight(int flight_num, FlightType flight_type, char destination[], BOOL emergency){
+	
+	FLIGHT* temp_flight = (FLIGHT*)malloc(sizeof(FLIGHT));
+	temp_flight->flight_num=flight_num;
+	temp_flight->flight_type=flight_type;
+	temp_flight->destination=destination;
+	temp_flight->emergency=emergency;
+	
+	return temp_flight;
+}
+
+void destroyFlight(*FLIGHT flight);
+void printFlight(*FLIGHT flight);
+
--- flight.h
+++ flight.h
@@ -1,19 +1,15 @@
 /* flight.h - intarface to an Flight ADT which implements a singal flight*/
 
-//*************************************************************************
-//* Function name:
-//* Description:
-//* Parameters:
-//* Return Value:
-//*************************************************************************
 
 #ifndef FLIGHT_H_
 #define FLIGHT_H_
 
+#include <AirPort_Include_File.h>
+
 typedef struct flight_t FLIGHT;
 
-FLIGHT createFlight{int flight_num, FlightType flight_type, char destination[], BOOL emergency};
-void destroyFlight{*FLIGHT flight};
-void printFlight{*FLIGHT flight};
+FLIGHT* createFlight(int flight_num, FlightType flight_type, char destination[], BOOL emergency);
+void destroyFlight(*FLIGHT flight);
+void printFlight(*FLIGHT flight);
 
 #endif /* FLIGHT_H_*/
